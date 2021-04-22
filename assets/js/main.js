758bd06d47cdbbe89894d2f3fe44ee0ff62fd43e
Suresh Kumar <suresh@suresh.kumar.successivetech.com>
2021-04-22 11:38:20 +0530

    modification done

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,25 +1,34 @@
-// crousel jquery
 $('.center').slick({
     centerMode: true,
-    centerPadding: '60px',
-    slidesToShow: 3,
+    centerPadding: '250px',
+    slidesToShow: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
     responsive: [{
             breakpoint: 768,
             settings: {
                 arrows: false,
-                centerMode: true,
-                centerPadding: '40px',
-                slidesToShow: 3
+                centerMode: false,
+                centerPadding: '10px',
+                slidesToShow: 1
             }
         },
         {
             breakpoint: 480,
             settings: {
                 arrows: false,
-                centerMode: true,
-                centerPadding: '40px',
+                centerMode: false,
+                centerPadding: '75px',
                 slidesToShow: 1
             }
         }
     ]
+});
+//image slider
+$('.card_six').slick({
+    infinite: true,
+    speed: 500,
+    cssEase: 'linear',
+    autoplay: true,
+    autoplaySpeed: 2000,
 });
